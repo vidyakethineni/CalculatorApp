b5d916304e60cfd7b3d2ee07808e0ff3579ae5d8
vkethine <vkethine@iu.edu>
2023-09-12 16:08:16 -0400

    Update commit 4:08 PM September 12, 2023

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -5,9 +5,12 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * MainActivity for the calculator application that replicates the functionality of the default iPhone calculator .
+ */
 class MainActivity : AppCompatActivity() {
-    private var textView: TextView? = null
-    private var currentNumber = ""
+    private lateinit var textView: TextView
+    private var currentNumber = StringBuilder()
     private var previousNumber = ""
     private var currentOperation: Char? = null
 
@@ -16,16 +19,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.textView)
 
-        // Click listeners for digits
+        /**
+         * Defining click listeners for the buttons in the Linear Layout
+         */
+
+        /**
+         * Click listeners for digits
+         */
         for (i in 0..9) {
             val buttonId = resources.getIdentifier("button$i", "id", packageName)
             val button = findViewById<Button>(buttonId)
             button.setOnClickListener { v ->
-                currentNumber += (v as Button).text
+                currentNumber.append((v as Button).text)
                 updateTextView()
             }
         }
 
+        /**
+         * Defining a map that pairs IDs of buttons in Linear Layout with their corresponding mathematical operator symbols
+         */
         val operatorButtons = mapOf(
             R.id.buttonDivide to '/',
             R.id.buttonMultiply to '*',
@@ -33,7 +45,10 @@ class MainActivity : AppCompatActivity() {
             R.id.buttonAdd to '+'
         )
 
-        // Click listener to insert respective operand
+        /**
+         * Click listener for operator symbols
+         * The performCalculation() helper function is called to calculate and output expressions
+         */
         operatorButtons.forEach { (buttonId, operator) ->
             val button = findViewById<Button>(buttonId)
             button.setOnClickListener {
@@ -41,93 +56,118 @@ class MainActivity : AppCompatActivity() {
                     if (currentOperation != null) {
                         performCalculation()
                     }
-                    previousNumber = currentNumber
-                    currentNumber = ""
+                    previousNumber = currentNumber.toString()
+                    currentNumber.setLength(0)
                     currentOperation = operator
-                    updateTextView(previousNumber)
+                    updateTextView()
                 }
             }
         }
 
-        // Click listener to insert decimal
+        /**
+         * Click listener to insert decimal
+         */
         val buttonDecimal = findViewById<Button>(R.id.buttonDecimal)
         buttonDecimal.setOnClickListener {
             if (!currentNumber.contains(".")) {
-                currentNumber += "."
+                currentNumber.append(".")
                 updateTextView()
             }
         }
 
-        // Click listener to evaluate expressions
+        /**
+         * Click listener for equal sign, which calls the performCalculation() helper function to calculate and output expressions
+         */
         val buttonEquals = findViewById<Button>(R.id.buttonEquals)
         buttonEquals.setOnClickListener {
             if (currentNumber.isNotEmpty() && previousNumber.isNotEmpty() && currentOperation != null) {
                 performCalculation()
                 currentOperation = null
+                previousNumber = "" // Clear the previousNumber after calculation
             }
         }
 
-        // Click listener to clear text view
+        /**
+         * Click listener to clear and reset text view to 0
+         */
         val buttonClear = findViewById<Button>(R.id.buttonClear)
         buttonClear.setOnClickListener {
-            currentNumber = ""
-            previousNumber = ""
-            currentOperation = null
-            updateTextView()
+            clearCalculator()
         }
 
-        // Click listener to change number from positive to negative, or negative to positive
+        /**
+         * Click listener to change number from positive to negative, or negative to positive
+         */
         val buttonArithmetic = findViewById<Button>(R.id.buttonArithmetic)
         buttonArithmetic.setOnClickListener {
             if (currentNumber.isNotEmpty()) {
-                currentNumber = if (currentNumber.startsWith("-")) {
-                    currentNumber.substring(1)
+                if (currentNumber[0] == '-') {
+                    currentNumber.deleteCharAt(0)
                 } else {
-                    "-$currentNumber"
+                    currentNumber.insert(0, "-")
                 }
                 updateTextView()
             }
         }
 
-        // Click listener to find the decimal value of a number
+        /**
+         * Click listener to find the decimal value of an inputted number
+         */
         val buttonPercentage = findViewById<Button>(R.id.buttonPercentage)
         buttonPercentage.setOnClickListener {
             if (currentNumber.isNotEmpty()) {
-                val number = currentNumber.toDouble()
+                val number = currentNumber.toString().toDoubleOrNull() ?: return@setOnClickListener
                 val percentage = number / 100.0
-                currentNumber = percentage.toString()
+                currentNumber.setLength(0)
+                currentNumber.append(percentage.toString())
                 updateTextView()
             }
         }
-
     }
 
-    // Helper function for updating text view
-    private fun updateTextView(text: String? = null) {
-        val displayText = text ?: if (currentNumber.isNotEmpty()) currentNumber else "0"
-
-        // Format the number based on whether it's a whole number or not
-        val isWholeNumber = currentNumber.toDouble() % 1 == 0.0
-        textView!!.text = if (isWholeNumber) {
-            displayText.toInt().toString()
-        } else {
-            displayText
+    /**
+     * Helper function for updating text view
+     */
+    private fun updateTextView() {
+        val displayText = if (currentNumber.isNotEmpty()) currentNumber.toString() else if (previousNumber.isNotEmpty()) previousNumber else "0"
+        val formattedText = when {
+            displayText.endsWith(".0") -> displayText.substring(0, displayText.length - 2)
+            else -> displayText
         }
+        textView.text = formattedText
     }
 
-    // Helper function for evaluating expressions
+
+    /**
+     * Helper function for evaluating expressions based on the operator, and handles division by zero
+     */
     private fun performCalculation() {
-        val num1 = previousNumber.toDouble()
-        val num2 = currentNumber.toDouble()
-        val result = when (currentOperation) {
-            '+' -> num1 + num2
-            '-' -> num1 - num2
-            '*' -> num1 * num2
-            '/' -> num1 / num2
+        val num1 = previousNumber.toDoubleOrNull() ?: return
+        val num2 = currentNumber.toString().toDoubleOrNull() ?: return
+
+        when (currentOperation) {
+            '+' -> currentNumber = StringBuilder((num1 + num2).toString())
+            '-' -> currentNumber = StringBuilder((num1 - num2).toString())
+            '*' -> currentNumber = StringBuilder((num1 * num2).toString())
+            '/' -> {
+                if (num2 != 0.0) {
+                    currentNumber = StringBuilder((num1 / num2).toString())
+                } else {
+                    currentNumber = StringBuilder("Error")
+                }
+            }
             else -> return
         }
-        currentNumber = result.toString()
+        updateTextView()
+    }
+
+    /**
+     * Helper function to clear the calculator
+     */
+    private fun clearCalculator() {
+        currentNumber.setLength(0)
         previousNumber = ""
-        updateTextView(currentNumber)
+        currentOperation = null
+        updateTextView()
     }
 }
