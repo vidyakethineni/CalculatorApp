9aeb1ed1816b0ee09b89c514fb68480fffd5d0f4
vkethine <vkethine@iu.edu>
2023-09-24 19:01:42 -0400

    Update commit 7:01 PM September 24, 2023

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -5,9 +5,12 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.isDigitsOnly
+import android.content.res.Configuration
+import android.util.Log
+
 
 /**
- * MainActivity for the calculator application that replicates the functionality of the default iPhone calculator .
+ * MainActivity for the application that replicates the functionality of a calculator.
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
@@ -15,13 +18,177 @@ class MainActivity : AppCompatActivity() {
     private var currentOperation: Char? = null
     private var expression = ArrayList<String>()
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // Save the currentNumber and expression
+        outState.putString("currentNumber", currentNumber.toString())
+        outState.putStringArrayList("expression", expression)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        // Restore the currentNumber and expression
+        currentNumber = StringBuilder(savedInstanceState.getString("currentNumber", ""))
+        expression = savedInstanceState.getStringArrayList("expression") ?: ArrayList()
+        updateTextView()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        /**
+         * Determining the current orientation (i.e vertical or horizontal)
+         */
+        val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+        /**
+         * Inflating the appropriate layout XML file based on orientation
+         */
+        val layoutId = if (isLandscape) R.layout.activity_horizontal else R.layout.activity_main
+        setContentView(layoutId)
+
+        //setContentView(R.layout.activity_main)
         textView = findViewById(R.id.textView)
 
         /**
-         * Defining click listeners for the buttons in the Linear Layout
+         * Defining additional functionality of application for the horizontal orientation
+         */
+        if (isLandscape) {
+            /**
+             * Restoring user input (if it exists) after orientation change
+             */
+            savedInstanceState?.let {
+                currentNumber = StringBuilder(it.getString("currentNumber", ""))
+                expression = it.getStringArrayList("expression") ?: ArrayList()
+                updateTextView()
+            }
+            /**
+             * Logging button clicks using LogCat
+             */
+            val buttons = arrayOf<Button>(
+                // List of buttons present only in the horizontal orientation
+                findViewById(R.id.buttonSin),
+                findViewById(R.id.buttonCos),
+                findViewById(R.id.buttonTan),
+                findViewById(R.id.buttonLog10),
+                findViewById(R.id.buttonLn)
+            )
+            /**
+             * Setting click listeners for buttons present only in the horizontal orientation
+             */
+            // Set a click listener for the sin button
+            val buttonSin = findViewById<Button>(R.id.buttonSin)
+            buttonSin.setOnClickListener {
+                Log.d("ButtonClicked", "sin button clicked")
+
+                if (currentNumber.isNotEmpty()) {
+                    val number =
+                        currentNumber.toString().toDoubleOrNull() ?: return@setOnClickListener
+                    val result = Math.sin(Math.toRadians(number))
+                    currentNumber.setLength(0)
+                    currentNumber.append(result.toString())
+                    expression.removeAt(expression.size - 1)
+                    expression.add(result.toString())
+                    updateTextView()
+                }
+            }
+            // Set a click listener for the cos button
+            val buttonCos = findViewById<Button>(R.id.buttonCos)
+            buttonCos.setOnClickListener {
+                Log.d("ButtonClicked", "cos button clicked")
+
+                if (currentNumber.isNotEmpty()) {
+                    val number =
+                        currentNumber.toString().toDoubleOrNull() ?: return@setOnClickListener
+                    val result = Math.cos(Math.toRadians(number))
+                    currentNumber.setLength(0)
+                    currentNumber.append(result.toString())
+                    expression.removeAt(expression.size - 1)
+                    expression.add(result.toString())
+                    updateTextView()
+                }
+            }
+            // Set a click listener for the tan button
+            val buttonTan = findViewById<Button>(R.id.buttonTan)
+            buttonTan.setOnClickListener {
+                Log.d("ButtonClicked", "tan button clicked")
+
+                if (currentNumber.isNotEmpty()) {
+                    val number =
+                        currentNumber.toString().toDoubleOrNull() ?: return@setOnClickListener
+                    val result = Math.tan(Math.toRadians(number))
+                    currentNumber.setLength(0)
+                    currentNumber.append(result.toString())
+                    expression.removeAt(expression.size - 1)
+                    expression.add(result.toString())
+                    updateTextView()
+                }
+            }
+            // Set a click listener for the Log 10 button
+            val buttonLog10 = findViewById<Button>(R.id.buttonLog10)
+            buttonLog10.setOnClickListener {
+                Log.d("ButtonClicked", "Log 10 button clicked")
+
+                if (currentNumber.isNotEmpty()) {
+                    val number =
+                        currentNumber.toString().toDoubleOrNull() ?: return@setOnClickListener
+                    val result = Math.log10(number)
+                    currentNumber.setLength(0)
+                    currentNumber.append(result.toString())
+                    expression.removeAt(expression.size - 1)
+                    expression.add(result.toString())
+                    updateTextView()
+                }
+            }
+            // Set a click listener for the ln button
+            val buttonLn = findViewById<Button>(R.id.buttonLn)
+            buttonLn.setOnClickListener {
+                Log.d("ButtonClicked", "ln button clicked")
+
+                if (currentNumber.isNotEmpty()) {
+                    val number =
+                        currentNumber.toString().toDoubleOrNull() ?: return@setOnClickListener
+                    val result = Math.log(number)
+                    currentNumber.setLength(0)
+                    currentNumber.append(result.toString())
+                    expression.removeAt(expression.size - 1)
+                    expression.add(result.toString())
+                    updateTextView()
+                }
+            }
+        }
+
+
+        /**
+         * Logging button clicks using LogCat
+         */
+        val buttons = arrayOf<Button>(
+            // List all your buttons here
+            findViewById(R.id.button0),
+            findViewById(R.id.button1),
+            findViewById(R.id.button2),
+            findViewById(R.id.button3),
+            findViewById(R.id.button4),
+            findViewById(R.id.button5),
+            findViewById(R.id.button6),
+            findViewById(R.id.button7),
+            findViewById(R.id.button8),
+            findViewById(R.id.button9),
+            findViewById(R.id.buttonAdd),
+            findViewById(R.id.buttonSubtract),
+            findViewById(R.id.buttonMultiply),
+            findViewById(R.id.buttonDivide),
+            findViewById(R.id.buttonDecimal),
+            findViewById(R.id.buttonArithmetic),
+            findViewById(R.id.buttonPercentage),
+            findViewById(R.id.buttonEquals),
+            findViewById(R.id.buttonClear)
+        )
+
+        /**
+         * Defining click listeners for the buttons in the vertical and horizontal orientations
          */
 
         /**
@@ -32,6 +199,9 @@ class MainActivity : AppCompatActivity() {
             val button = findViewById<Button>(buttonId)
             button.setOnClickListener { v ->
                 val digit = (v as Button).text.toString()
+
+                Log.d("ButtonClicked", digit + " button clicked")
+
                 if (expression.isNotEmpty() && expression.last().isDigitsOnly()) {
                     // Concatenate the digit to the last number
                     expression[expression.size-1] += digit
@@ -62,6 +232,7 @@ class MainActivity : AppCompatActivity() {
         operatorButtons.forEach { (buttonId, operator) ->
             val button = findViewById<Button>(buttonId)
             button.setOnClickListener {
+                Log.d("ButtonClicked", operator + " button clicked")
                 currentOperation = operator
                 expression.add(operator.toString())
                 currentNumber.setLength(0)
@@ -73,6 +244,7 @@ class MainActivity : AppCompatActivity() {
          */
         val buttonDecimal = findViewById<Button>(R.id.buttonDecimal)
         buttonDecimal.setOnClickListener {
+            Log.d("ButtonClicked","Decimal button clicked")
             if (!currentNumber.contains(".")) {
                 expression.add(".")
                 currentNumber.append(".")
@@ -85,6 +257,8 @@ class MainActivity : AppCompatActivity() {
          */
         val buttonEquals = findViewById<Button>(R.id.buttonEquals)
         buttonEquals.setOnClickListener {
+            Log.d("ButtonClicked","Equal button clicked")
+
             //First completing multiplication expressions
             var i = 0
             while (i < expression.size) {
@@ -149,6 +323,8 @@ class MainActivity : AppCompatActivity() {
          */
         val buttonClear = findViewById<Button>(R.id.buttonClear)
         buttonClear.setOnClickListener {
+            Log.d("ButtonClicked","Clear button clicked")
+
             clearCalculator()
         }
 
@@ -157,6 +333,8 @@ class MainActivity : AppCompatActivity() {
          */
         val buttonArithmetic = findViewById<Button>(R.id.buttonArithmetic)
         buttonArithmetic.setOnClickListener {
+            Log.d("ButtonClicked","Arithmetic sign button clicked")
+
             if (currentNumber.isNotEmpty()) {
                 // Toggle the sign of the current number
                 val firstChar = currentNumber[0]
@@ -176,6 +354,8 @@ class MainActivity : AppCompatActivity() {
          */
         val buttonPercentage = findViewById<Button>(R.id.buttonPercentage)
         buttonPercentage.setOnClickListener {
+            Log.d("ButtonClicked","Percentage button clicked")
+
             if (currentNumber.isNotEmpty()) {
                 val number = currentNumber.toString().toDoubleOrNull() ?: return@setOnClickListener
                 val percentage = number / 100.0
